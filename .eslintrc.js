affaffe193d737e8755c5cc6a8b00a796faa9762
Akash <akash.p@optisolbusiness.com>
2024-08-12 23:20:22 +0530

    config env and implemented esLint prettier for code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error', // Ensure Prettier errors are shown as ESLint errors
+    // Add your custom rules here
+  },
 };
--- src/services/Api/apiConfig.tsx
+++ src/services/Api/apiConfig.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 // Create an axios instance
 const apiConfig = axios.create({
-  baseURL: 'https://api.yourservice.com', // replace with your API base URL
+  baseURL: Config.Testing_URL, // replace with your API base URL
   timeout: 10000, // request timeout
 });
 export default apiConfig;
