e7e1a27efe35cf3ba2af6009d30fc779a8991c15
Akash <akash.p@optisolbusiness.com>
2024-08-28 15:35:06 +0530

    google signin completed

--- src/screens/Auth/registerScreen.tsx
+++ src/screens/Auth/registerScreen.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {AppStyles} from '../../utils/styles/appStyles';
 import Button from '../../components/commonComponents/button';
-
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {androidClient, iOSClient} from '../../utils/constant/commonConstant';
 export const RegisterScreen = ({navigation}) => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'],
+      webClientId: androidClient,
+      iosClientId: iOSClient,
+      offlineAccess: true,
+    });
+  }, []);
+  const googleSignup = async () => {
+    try {
+      await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+      // const userout = await GoogleSignin.signOut();
+      // console.log('userDetails-->', userout);
+      const userDetails = await GoogleSignin.signIn();
+      let user = userDetails?.user;
+      let userInfo = {
+        googleFirstName: user?.givenName,
+        googleLastName: user?.familyName,
+        email: user?.email,
+      };
+      console.log('GoogleSignin', userInfo);
+      navigation.navigate('Login');
+    } catch (error) {
+      console.log('GoogleSignin error', error);
+    }
+  };
   return (
     <View style={AppStyles.container}>
-      <Button onPress={() => navigation.navigate('Login')} title={'SignUp'} />
+      <Button onPress={() => googleSignup()} title={'SignUp'} />
     </View>
   );
 };
---
+++ src/utils/constant/commonConstant.js
@@ -0,0 +1,4 @@
+export const androidClient =
+  '262843769240-j3e68f5pu3s94qqm2376ls9ri37q3e9n.apps.googleusercontent.com';
+export const iOSClient =
+  '262843769240-pppu05fr3kqlh79ftrct4a5jlj1jvhui.apps.googleusercontent.com';
